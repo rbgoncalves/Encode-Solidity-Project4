9dac632aa76cac66efaf3eec79374684d1ce954a
rbgoncalves <r.goncalves@xgeeks.io>
2022-06-05 17:30:41 +0100

    feat: emit event on Nftmint

--- contracts/RealMonkeyCollection.sol
+++ contracts/RealMonkeyCollection.sol
@@ -5,17 +5,20 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RealMonkeyCollection is ERC721URIStorage, Ownable {
+    event NftMinted(address indexed to, uint256 tokenId, string tokenURI);
 
     constructor() public ERC721("Real Monkey Collection", "RMC") {}
 
-    function mintNft(address to, uint256 id, string memory tokenURI)
+    function mintNft(address to, uint256 tokenId, string memory tokenURI)
         public
         onlyOwner
         returns (uint256)
     {
-        _mint(to, id);
-        _setTokenURI(id, tokenURI);
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
 
-        return id;
+        emit NftMinted(to, tokenId, tokenURI);
+
+        return tokenId;
     }
 }
